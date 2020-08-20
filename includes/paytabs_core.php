64d412a03c93c26192e068a095c8707a45383d33
wajih <wajih.km@gmail.com>
2020-08-20 10:31:21 +0400

    improve fixing sum of total amount issue

--- includes/paytabs_core.php
+++ includes/paytabs_core.php
@@ -2,7 +2,7 @@
 
 /**
  * PayTabs PHP SDK
- * Version: 1.2.1
+ * Version: 1.2.2
  */
 
 
@@ -798,11 +798,11 @@ class PaytabsHolder
 
         $sums += $other_charges;
 
-        $diff = $amount - $sums;
+        $diff = round($amount - $sums, 2);
         if ($diff != 0) {
             $_logParams = json_encode($pay);
 
-            if (abs($diff) > self::THRESHOLD) {
+            if (abs($diff) > self::THRESHOLD && self::THRESHOLD >= 0) {
                 PaytabsHelper::log("PaytabsHelper::round_amount: diff = {$diff}, [{$_logParams}]", 3);
             } else {
                 PaytabsHelper::log("PaytabsHelper::round_amount: diff = {$diff} added to 'other_charges', [{$_logParams}]", 2);
