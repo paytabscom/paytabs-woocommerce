673f7a73bd0fd042f9569eeec5b6c6f6e21955ed
Wajih <wajih.km@gmail.com>
2023-06-14 14:48:16 +0400

    Refactor code

--- includes/paytabs_payment_token.php
+++ includes/paytabs_payment_token.php
@@ -48,9 +48,6 @@ class WC_Payment_Token_PayTabs extends WC_Payment_Token
     {
         $this->set_prop('tran_ref', $tran_ref);
     }
-    public function get_last4( $context = 'view' ) {
-        return $this->get_prop( 'last4', $context );
-    }
 
     /**
      * Set the last four digits.
@@ -59,8 +56,14 @@ class WC_Payment_Token_PayTabs extends WC_Payment_Token
      * @version 4.0.0
      * @param string $last4
      */
-    public function set_last4( $last4 ) {
-        $this->set_prop( 'last4', $last4 );
+    public function set_last4($last4)
+    {
+        $this->set_prop('last4', $last4);
+    }
+
+    public function get_last4($context = 'view')
+    {
+        return $this->get_prop('last4', $context);
     }
 
     /**
@@ -68,8 +71,9 @@ class WC_Payment_Token_PayTabs extends WC_Payment_Token
      *
      * @param string $type Payment method type.
      */
-    public function set_payment_method_type( $type ) {
-        $this->set_prop( 'payment_method_type', $type );
+    public function set_payment_method_type($type)
+    {
+        $this->set_prop('payment_method_type', $type);
     }
 
     /**
@@ -78,12 +82,11 @@ class WC_Payment_Token_PayTabs extends WC_Payment_Token
      * @param string $context What the value is for. Valid values are view and edit.
      * @return string $payment_method_type
      */
-    public function get_payment_method_type( $context = 'view' ) {
-        return $this->get_prop( 'payment_method_type', $context );
+    public function get_payment_method_type($context = 'view')
+    {
+        return $this->get_prop('payment_method_type', $context);
     }
-    public function get_card_type( $context = 'view' ) {
-        return $this->get_prop( 'card_type', $context );
-    }
+
 
     /**
      * Set the card type (mastercard, visa, ...).
@@ -91,11 +94,13 @@ class WC_Payment_Token_PayTabs extends WC_Payment_Token
      * @since 2.6.0
      * @param string $type Credit card type (mastercard, visa, ...).
      */
-    public function set_card_type( $type ) {
-        $this->set_prop( 'card_type', $type );
+    public function set_card_type($type)
+    {
+        $this->set_prop('card_type', $type);
     }
-    public function get_expiry_year( $context = 'view' ) {
-        return $this->get_prop( 'expiry_year', $context );
+    public function get_card_type($context = 'view')
+    {
+        return $this->get_prop('card_type', $context);
     }
 
     /**
@@ -104,13 +109,13 @@ class WC_Payment_Token_PayTabs extends WC_Payment_Token
      * @since 2.6.0
      * @param string $year Credit card expiration year.
      */
-    public function set_expiry_year( $year ) {
-        $this->set_prop( 'expiry_year', $year );
+    public function set_expiry_year($year)
+    {
+        $this->set_prop('expiry_year', $year);
     }
-
-
-    public function get_expiry_month( $context = 'view' ) {
-        return $this->get_prop( 'expiry_month', $context );
+    public function get_expiry_year($context = 'view')
+    {
+        return $this->get_prop('expiry_year', $context);
     }
 
     /**
@@ -119,28 +124,39 @@ class WC_Payment_Token_PayTabs extends WC_Payment_Token
      * @since 2.6.0
      * @param string $month Credit card expiration month.
      */
-    public function set_expiry_month( $month ) {
-        $this->set_prop( 'expiry_month', str_pad( $month, 2, '0', STR_PAD_LEFT ) );
+    public function set_expiry_month($month)
+    {
+        $this->set_prop('expiry_month', str_pad($month, 2, '0', STR_PAD_LEFT));
+    }
+    public function get_expiry_month($context = 'view')
+    {
+        return $this->get_prop('expiry_month', $context);
     }
 
-    public function get_gateway_id( $context = 'view' ) {
-        return $this->get_prop( 'gateway_id', $context );
+    public function get_gateway_id($context = 'view')
+    {
+        return $this->get_prop('gateway_id', $context);
     }
-    public function set_gateway_id( $gateway_id ) {
-        $this->set_prop( 'gateway_id', $gateway_id );
+    public function set_gateway_id($gateway_id)
+    {
+        $this->set_prop('gateway_id', $gateway_id);
     }
 }
 
 
-class WC_Paytabs_Payment_Tokens extends WC_Payment_Token_PayTabs{
-    public function __construct() {
-        add_filter( 'woocommerce_payment_methods_list_item', [ $this, 'get_account_saved_payment_methods_list_item_paytabs' ], 10, 2 );
+class WC_Paytabs_Payment_Tokens extends WC_Payment_Token_PayTabs
+{
+    public function __construct()
+    {
+        add_filter('woocommerce_payment_methods_list_item', [$this, 'get_account_saved_payment_methods_list_item_paytabs'], 10, 2);
     }
-    public function get_account_saved_payment_methods_list_item_paytabs( $item, $payment_token ) {
-        if ( 'paytabs' === strtolower( $payment_token->get_type() ) ) {
+
+    public function get_account_saved_payment_methods_list_item_paytabs($item, $payment_token)
+    {
+        if ('paytabs' === strtolower($payment_token->get_type())) {
             $item['method']['last4'] = $payment_token->get_last4();
-            $item['method']['expire'] = $this->get_expiry_year();//
-            $item['method']['brand'] = isset($item['method']['gateway'])?$item['method']['gateway']:'Paytabs,'.$this->get_display_name();
+            $item['method']['expire'] = $this->get_expiry_year(); //
+            $item['method']['brand'] = isset($item['method']['gateway']) ? $item['method']['gateway'] : 'Paytabs,' . $this->get_display_name();
         }
         return $item;
     }
