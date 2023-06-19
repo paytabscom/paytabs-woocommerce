acece63a016e7efa63e80f829c58596fa96bd956
Wajih <wajih.km@gmail.com>
2023-06-19 10:33:12 +0400

    Refactor

--- includes/paytabs_payment_token.php
+++ includes/paytabs_payment_token.php
@@ -98,6 +98,7 @@ class WC_Payment_Token_PayTabs extends WC_Payment_Token
     {
         $this->set_prop('card_type', $type);
     }
+
     public function get_card_type($context = 'view')
     {
         return $this->get_prop('card_type', $context);
@@ -113,6 +114,7 @@ class WC_Payment_Token_PayTabs extends WC_Payment_Token
     {
         $this->set_prop('expiry_year', $year);
     }
+
     public function get_expiry_year($context = 'view')
     {
         return $this->get_prop('expiry_year', $context);
@@ -128,18 +130,10 @@ class WC_Payment_Token_PayTabs extends WC_Payment_Token
     {
         $this->set_prop('expiry_month', str_pad($month, 2, '0', STR_PAD_LEFT));
     }
+
     public function get_expiry_month($context = 'view')
     {
         return $this->get_prop('expiry_month', $context);
-    }
-
-    public function get_gateway_id($context = 'view')
-    {
-        return $this->get_prop('gateway_id', $context);
-    }
-    public function set_gateway_id($gateway_id)
-    {
-        $this->set_prop('gateway_id', $gateway_id);
     }
 }
 
@@ -155,8 +149,8 @@ class WC_Paytabs_Payment_Tokens extends WC_Payment_Token_PayTabs
     {
         if ('paytabs' === strtolower($payment_token->get_type())) {
             $item['method']['last4'] = $payment_token->get_last4();
-            $item['method']['expire'] = $this->get_expiry_year(); //
-            $item['method']['brand'] = isset($item['method']['gateway']) ? $item['method']['gateway'] : 'Paytabs,' . $this->get_display_name();
+            $item['method']['expire'] = $this->get_expiry_year();
+            $item['method']['brand'] = isset($item['method']['gateway']) ? $item['method']['gateway'] : ('PayTabs, ' . $this->get_display_name());
         }
         return $item;
     }
