b3fc28681e0bdcd91166c3644bef068ab79da095
Wajih <wajih.km@gmail.com>
2023-09-30 06:48:38 +0000

    Use higher resolution icon for valU ENhance the widget HTML

--- includes/_valu_widget.php
+++ includes/_valu_widget.php
@@ -6,7 +6,15 @@ $asset_url = $plugin_directory_url . '/icons/valu.png';
 
 ?>
 
-<div class="paytabs_valu_widget" style="float: right; width: 48%; clear: none;">
-    <img src="<?= esc_url($asset_url) ?>" alt="Valu Logo">
-    <p style="display: inline-block; margin-left: 2%;">Pay 3 interest-free payments of EGP <?= $plan['emi'] ?>.</p>
+<div style="border: 1px solid orange; border-radius: 12px; margin: 10px 0px;">
+    <table>
+        <tr>
+            <td style="width: 1%; white-space: nowrap;">
+                <img src="<?= esc_url($asset_url) ?>" alt="valU logo" style="min-height: 64px; max-width: 128px;">
+            </td>
+            <td style="vertical-align: middle;">
+                Pay 3 interest-free payments of EGP <?= $plan['emi'] ?>
+            </td>
+        </tr>
+    </table>
 </div>
