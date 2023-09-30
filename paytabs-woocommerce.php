56941c157cc551c09aecba2f7dd7ccb78a6ca1ca
Wajih <wajih.km@gmail.com>
2023-09-30 07:50:35 +0000

    Move the widget to its own class

--- paytabs-woocommerce.php
+++ paytabs-woocommerce.php
@@ -53,29 +53,6 @@ define('PAYTABS_PAYPAGE_METHODS', [
 
 //load plugin function when woocommerce loaded
 add_action('plugins_loaded', 'woocommerce_paytabs_init', 0);
-add_action('woocommerce_single_product_summary', 'valu_widget', 30);
-
-function valu_widget()
-{
-  $enabled_gateways = WC()->payment_gateways->get_available_payment_gateways();
-
-  if (array_key_exists('paytabs_valu', $enabled_gateways)) {
-    $valu_payment = $enabled_gateways['paytabs_valu'];
-    if ($valu_payment->valu_widget_enable) {
-
-      $product_price = get_product_price();
-      if ($product_price) {
-        if ($product_price >= $valu_payment->valu_widget_price_threshold) {
-          $plan = call_valu_api($valu_payment, $product_price);
-
-          if ($plan) {
-            include('includes/_valu_widget.php');
-          }
-        }
-      }
-    }
-  }
-}
 
 
 function get_product_price()
@@ -93,71 +70,6 @@ function get_product_price()
   return $product_price;
 }
 
-function call_valu_api($valu_payment, $product_price)
-{
-  $_paytabsApi = PaytabsApi::getInstance($valu_payment->paytabs_endpoint, $valu_payment->merchant_id, $valu_payment->merchant_key);
-  $phone_number = $valu_payment->valu_widget_phone_number;
-
-  $data = [
-    'cart_amount' => $product_price,
-    'cart_currency' => "EGP",
-    'customer_details' => [
-      "phone" => $phone_number
-    ],
-  ];
-
-  PaytabsHelper::log("valU inqiry, {$product_price}", 1);
-  $details = $_paytabsApi->inqiry_valu($data);
-
-  if (!$details || !$details->success) {
-    $_err_msg = json_encode($details);
-    PaytabsHelper::log("valU Details error: [{$_err_msg}]", 3);
-    return false;
-  }
-
-  $installments_count = 3;
-  $valu_plan = getValUPlan($details, $installments_count);
-
-  if (!$valu_plan) {
-    return false;
-  }
-
-  try {
-    $installment_amount = $valu_plan->emi;
-
-    $calculated_installment = round($product_price / $installments_count, 2);
-    $is_free_interest = $calculated_installment >= $installment_amount;
-
-    $txt_free = $is_free_interest ? "interest-free" : "";
-
-    $msg = "Pay {$installments_count} {$txt_free} payments of EGP $installment_amount.";
-
-    return $msg;
-  } catch (\Throwable $th) {
-    PaytabsHelper::log("valU widget error: " . $th->getMessage(), 3);
-  }
-
-  return false;
-}
-
-function getValUPlan($details, $installments_count)
-{
-  try {
-    $plansList = $details->valuResponse->productList[0]->tenureList;
-    foreach ($plansList as $plan) {
-      if ($plan->tenorMonth == $installments_count) {
-        return $plan;
-      }
-    }
-  } catch (\Throwable $th) {
-    PaytabsHelper::log("valU Plan error: " . $th->getMessage(), 3);
-  }
-
-  $_log = json_encode($plansList);
-  PaytabsHelper::log("valU Plan error: No Plan selected, [{$_log}]", 2);
-
-  return false;
-}
 
 function woocommerce_paytabs_init()
 {
@@ -175,6 +87,7 @@ function woocommerce_paytabs_init()
   require_once PAYTABS_PAYPAGE_DIR . 'includes/paytabs_payment_methods.php';
   require_once PAYTABS_PAYPAGE_DIR . 'includes/paytabs_gateways.php';
   require_once PAYTABS_PAYPAGE_DIR . 'includes/paytabs_payment_token.php';
+  require_once PAYTABS_PAYPAGE_DIR . 'includes/widgets/valu.php';
 
 
   /**
@@ -231,4 +144,18 @@ function woocommerce_paytabs_init()
   add_filter('woocommerce_payment_gateways', 'paytabs_filter_gateways', 10, 1);
   add_filter('woocommerce_payment_methods_list_item', 'get_account_saved_payment_methods_list_item_paytabs', 10, 2);
   add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'paytabs_add_action_links');
+
+  add_action('woocommerce_single_product_summary', 'valu_widget', 20);
+
+  function valu_widget()
+  {
+    $enabled_gateways = WC()->payment_gateways->get_available_payment_gateways();
+
+    if (array_key_exists('paytabs_valu', $enabled_gateways)) {
+      $valu_payment = $enabled_gateways['paytabs_valu'];
+
+      $valu_widget = new ValuWidget();
+      $valu_widget->init($valu_payment);
+    }
+  }
 }
