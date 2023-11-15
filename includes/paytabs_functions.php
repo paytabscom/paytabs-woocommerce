463201df07b00a5f6e03ddc8de11d45dafce0759
Walaa Elsaeed <79043296+Walaa-Elsaeed-93@users.noreply.github.com>
2023-11-15 08:25:55 +0200

    Htaccess (#59)
    * Check debug file permission when activate the plugin only
    
    * Fix severity index
    
    * PHP SDK update

--- includes/paytabs_functions.php
+++ includes/paytabs_functions.php
@@ -24,3 +24,64 @@ function paytabs_error_log($message, $severity)
     $_prefix = date('c') . " PayTabs.{$severity_str}: ";
     error_log($_prefix . $message . PHP_EOL, 3, PAYTABS_DEBUG_FILE);
 }
+
+function woocommerce_paytabs_htaccess_notice()
+{
+    echo '<div class="notice notice-warning is-dismissible"><p>Ensure that you allow "<strong>override all</strong>" in your server\'s configurations to enable the proper functionality of the .htaccess file.</p></div>';
+}
+
+
+function woocommerce_paytabs_check_log_permission()
+{
+    // Print message to the merchant to make sure allow the webserver setting.
+    // add_action('admin_notices', 'woocommerce_paytabs_htaccess_notice');
+
+    $permission =
+        PHP_EOL .
+        "<Files " . PAYTABS_DEBUG_FILE_NAME . ">" . PHP_EOL .
+        "  Order Allow,Deny" . PHP_EOL .
+        "  Deny from all" . PHP_EOL .
+        "</Files>";
+    $htaccess_file_content = file_get_contents(PAYTABS_HTACCESS_FILE);
+
+    // prevent debug file from opening inside the browser
+    if (!file_exists(PAYTABS_HTACCESS_FILE)) {
+        $myhtaccessfile = fopen(PAYTABS_HTACCESS_FILE, "w");
+        $res = fwrite($myhtaccessfile, $permission);
+        fclose($myhtaccessfile);
+
+        if ($res) {
+            PaytabsHelper::log("Debug file secured.", 1);
+        } else {
+            PaytabsHelper::log("Could not write to .htaccess file.", 3);
+        }
+    } else {
+        // Try to read the content online
+        $url = PAYTABS_DEBUG_FILE_URL;
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_HEADER, true);    // we want headers
+        curl_setopt($ch, CURLOPT_NOBODY, true);    // we don't need body
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        @curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        @curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        $output = curl_exec($ch);
+        $error_num = curl_errno($ch);
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        if ($httpcode === 403) {
+            PaytabsHelper::log("Debug file already secured.", 1);
+        } elseif ($error_num) {
+            $output_err = curl_error($ch);
+            PaytabsHelper::log("Checking .htaccess error: [{$output_err}].", 2);
+        } elseif (strpos($htaccess_file_content, PAYTABS_DEBUG_FILE_NAME) !== false) {
+            PaytabsHelper::log("Allow 'override all' into your webserver to enable the proper functioning of the .htaccess file.", 2);
+        } else {
+            $htaccessFile = PAYTABS_HTACCESS_FILE;
+            file_put_contents($htaccessFile, $permission, FILE_APPEND);
+
+            PaytabsHelper::log("Debug file appended to htaccess.", 1);
+        }
+    }
+}
