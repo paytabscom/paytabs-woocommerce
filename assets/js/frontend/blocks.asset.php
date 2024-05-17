9fcdebac4c3328a944d841f1619b791c009d605f
Wajih <wajih.km@gmail.com>
2024-05-17 12:16:30 +0000

    Show the save token note

--- assets/js/frontend/blocks.asset.php
+++ assets/js/frontend/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => '1d1e241e59ead90c41ab');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => '42876f73af7e658f29e0');
