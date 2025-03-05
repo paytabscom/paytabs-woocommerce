e6ce75fcfc615c6816e88d9680515134af67324f
Wajih <wajih.km@gmail.com>
2025-03-05 09:54:50 +0000

    Version update Packages update

--- assets/js/frontend/blocks.asset.php
+++ assets/js/frontend/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => 'e7e5fc1b719107d77a08');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => '36a58bf9a6f684597cde');
