ce28c97a17207e398ba97c88ddf89ad7601d0298
Walaa Elsaeed <79043296+Walaa-Elsaeed-93@users.noreply.github.com>
2025-10-01 20:17:38 +0300

    revert reading version

--- includes/blocks/class-wc-paytabs-payments-blocks.php
+++ includes/blocks/class-wc-paytabs-payments-blocks.php
@@ -71,7 +71,7 @@ final class WC_Gateway_Paytabs_Blocks_Support extends AbstractPaymentMethodType
 			$this->id,
 			$script_url,
 			$script_asset['dependencies'],
-			time(),
+			$script_asset['version'],
 			true
 		);
 
