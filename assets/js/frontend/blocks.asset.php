39c44db3a70ac2ce6d1b8aa997e0c974978428df
Wajih <wajih.km@gmail.com>
2025-10-06 10:18:08 +0000

    Js build

--- assets/js/frontend/blocks.asset.php
+++ assets/js/frontend/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => '67abeb5d0a9fb7057fc1');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => 'adda7c8294be73d5afcf');
